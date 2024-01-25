48e6000419971f0900635eb029a98af234aada3f
dacm1999 <danielcontreras12.dc@gmail.com>
2024-01-25 17:15:31 +0100

    24 Actuator-Security Endpoints

---
+++ 01 - dev-spring-boot/01-spring-boot-overview/04-actuator-security-demo/src/main/java/com/dacm/springboot/demo/firstAppSpringBoot/FirstAppSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.dacm.springboot.demo.firstAppSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstAppSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstAppSpringBootApplication.class, args);
+	}
+
+}
