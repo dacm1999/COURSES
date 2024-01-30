be7e7d00a0eaa012812c48e0bf3b37eee34d269e
dacm1999 <danielcontreras12.dc@gmail.com>
2024-01-30 12:38:10 +0100

    Spring boot: 74 SAVING A JAVA OBJECT JPA

--- 04-SPRING-BOOT-COURSE/03-spring-boot-hibernate-jpa-crud/01-cruddemo-student-create/src/main/java/com/dacm/crudDemo/CrudDemoApplication.java
+++ 04-SPRING-BOOT-COURSE/03-spring-boot-hibernate-jpa-crud/01-cruddemo-student-create/src/main/java/com/dacm/crudDemo/CrudDemoApplication.java
@@ -1,5 +1,7 @@
 package com.dacm.crudDemo;
 
+import com.dacm.crudDemo.dao.StudentDAO;
+import com.dacm.crudDemo.entity.Student;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,12 +15,24 @@ public class CrudDemoApplication {
 	}
 
 	@Bean
-	public CommandLineRunner commandLineRunner() {
+	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
+
 
 		return runner -> {
+			createStudent(studentDAO);
 
-			System.out.println("Hey everyone");
 		};
+	}
+
+	private void createStudent(StudentDAO studentDAO) {
+		//Creating a new student
+		System.out.println("Creating a new student object...");
+		Student tempStuden = new Student("Daniel","Contreras", "danielcontreras@mail.conm");
+		//Save the object
+		System.out.println("Saving the student");
+		studentDAO.save(tempStuden);
+		//Display id of the save student
+		System.out.println("Saved student, Generate id" + tempStuden.getId());
 	}
 
 }
