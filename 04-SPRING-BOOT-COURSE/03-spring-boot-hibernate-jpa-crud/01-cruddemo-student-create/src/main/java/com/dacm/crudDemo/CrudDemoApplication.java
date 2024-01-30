a0d15789bf429dac45ae6d8059c6fe41c641d46a
dacm1999 <danielcontreras12.dc@gmail.com>
2024-01-30 10:45:33 +0100

    Spring boot: Setting Up project

--- 04-SPRING-BOOT-COURSE/03-spring-boot-hibernate-jpa-crud/01-cruddemo-student-create/src/main/java/com/dacm/crudDemo/CrudDemoApplication.java
+++ 04-SPRING-BOOT-COURSE/03-spring-boot-hibernate-jpa-crud/01-cruddemo-student-create/src/main/java/com/dacm/crudDemo/CrudDemoApplication.java
@@ -1,13 +1,24 @@
 package com.dacm.crudDemo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CrudDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrudDemoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+
+		return runner -> {
+
+			System.out.println("Hey everyone");
+		};
 	}
 
 }
