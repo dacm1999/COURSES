223d7ca50fe9ba33e78095e15844a0a2933927f6
dacm1999 <danielcontreras12.dc@gmail.com>
2024-02-01 12:57:11 +0100

    SPRING BOOT: 101 REST POJO

---
+++ 04-SPRING-BOOT-COURSE/04-spring-boot-rest-crud/01-spring-boot-rest-crud-hello-world/src/main/java/com/dacm/restfullservices/rest/StudentRestController.java
@@ -0,0 +1,29 @@
+package com.dacm.restfullservices.rest;
+
+import com.dacm.restfullservices.entity.Student;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class StudentRestController {
+
+    private List<Student> listStudents;
+
+    @GetMapping("/students")
+    public List<Student> getStudents(){
+
+        listStudents = new ArrayList<>();
+        listStudents.add(new Student("Daniel", "Contreras"));
+        listStudents.add(new Student("Luis", "Gomez"));
+        listStudents.add(new Student("Nicole", "Rojas"));
+        listStudents.add(new Student("Weptzel", "Morales"));
+        
+        return listStudents;
+    }
+
+}
