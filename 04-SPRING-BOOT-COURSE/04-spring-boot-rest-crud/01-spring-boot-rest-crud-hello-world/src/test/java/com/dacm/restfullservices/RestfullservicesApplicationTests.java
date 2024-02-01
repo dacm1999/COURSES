1c2dfef2df89571138092e0323d9eafa6a8eba03
dacm1999 <danielcontreras12.dc@gmail.com>
2024-02-01 11:17:19 +0100

    SPRING BOOT: 96 REST CONTROLLER

---
+++ 04-SPRING-BOOT-COURSE/04-spring-boot-rest-crud/01-spring-boot-rest-crud-hello-world/src/test/java/com/dacm/restfullservices/RestfullservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dacm.restfullservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfullservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
