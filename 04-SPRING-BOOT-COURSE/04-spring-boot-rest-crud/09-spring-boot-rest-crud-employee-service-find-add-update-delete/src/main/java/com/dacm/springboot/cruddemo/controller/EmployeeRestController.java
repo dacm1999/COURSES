6f0923a4a1f204ff3cc1d9504681e78a824f0dd7
dacm1999 <danielcontreras12.dc@gmail.com>
2024-02-02 13:46:37 +0100

    SPRING BOOT: 126 ADD, UPDATE AND DELETE

---
+++ 04-SPRING-BOOT-COURSE/04-spring-boot-rest-crud/09-spring-boot-rest-crud-employee-service-find-add-update-delete/src/main/java/com/dacm/springboot/cruddemo/controller/EmployeeRestController.java
@@ -0,0 +1,29 @@
+package com.dacm.springboot.cruddemo.controller;
+
+import com.dacm.springboot.cruddemo.entity.Employee;
+import com.dacm.springboot.cruddemo.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class EmployeeRestController {
+
+    // Quick and dirert: inject empleyee DAO
+
+    private EmployeeService employeeService;
+
+    @Autowired
+    public EmployeeRestController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping("/employees")
+    public List<Employee> findAll() {
+        return employeeService.findAll();
+    }
+}
