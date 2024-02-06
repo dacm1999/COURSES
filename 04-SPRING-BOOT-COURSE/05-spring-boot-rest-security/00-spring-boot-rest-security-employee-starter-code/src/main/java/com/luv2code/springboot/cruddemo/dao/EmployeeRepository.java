5c1e018bf8df0f38f10b986edf4399f33f0858af
dacm1999 <danielcontreras12.dc@gmail.com>
2024-02-06 11:44:57 +0100

    SPRING BOOT

--- 04-SPRING-BOOT-COURSE/05-spring-boot-rest-security/00-spring-boot-rest-security-employee-starter-code/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeRepository.java
+++ 04-SPRING-BOOT-COURSE/05-spring-boot-rest-security/00-spring-boot-rest-security-employee-starter-code/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeRepository.java
@@ -2,9 +2,7 @@ package com.luv2code.springboot.cruddemo.dao;
 
 import com.luv2code.springboot.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.webmvc.RepositoryRestController;
 
-//@RepositoryRestController(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
     // that's it ... no need to write any code LOL!
