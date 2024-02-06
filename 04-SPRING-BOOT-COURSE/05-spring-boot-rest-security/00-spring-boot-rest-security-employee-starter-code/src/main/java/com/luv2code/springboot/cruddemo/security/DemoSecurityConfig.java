bb4141548ce72b67fdbc683a1649d84a990240a5
dacm1999 <danielcontreras12.dc@gmail.com>
2024-02-06 12:34:24 +0100

    SPRING BOOT: 143 BASIC CONFIGURATION

--- 04-SPRING-BOOT-COURSE/05-spring-boot-rest-security/00-spring-boot-rest-security-employee-starter-code/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
+++ 04-SPRING-BOOT-COURSE/05-spring-boot-rest-security/00-spring-boot-rest-security-employee-starter-code/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -1,4 +1,33 @@
 package com.luv2code.springboot.cruddemo.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
 public class DemoSecurityConfig {
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager (){
+
+        UserDetails nicole = User.builder()
+                .username("nicole")
+                .password("admin")
+                .roles("EMPLOYEE")
+                .build();
+        UserDetails luis = User.builder()
+                .username("luis")
+                .password("{noop}12345")
+                .roles("EMPLOYEE", "MANAGER")
+                .build();
+        UserDetails daniel = User.builder()
+                .username("dacm")
+                .password("{noop}12345")
+                .roles("EMPLOYEE", "MANAGER", "ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(nicole, luis, daniel);
+    }
 }
