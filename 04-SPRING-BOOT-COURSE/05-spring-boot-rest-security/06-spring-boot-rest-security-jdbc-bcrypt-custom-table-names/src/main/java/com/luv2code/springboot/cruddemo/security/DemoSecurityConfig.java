e2e92e5ee4797bf7e3bd7d5717088090819f3c4f
dacm1999 <danielcontreras12.dc@gmail.com>
2024-02-07 11:37:08 +0100

    SPRING BOOT: 158 SECURITY CUSTOM TABLES

--- 04-SPRING-BOOT-COURSE/05-spring-boot-rest-security/06-spring-boot-rest-security-jdbc-bcrypt-custom-table-names/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
+++ 04-SPRING-BOOT-COURSE/05-spring-boot-rest-security/06-spring-boot-rest-security-jdbc-bcrypt-custom-table-names/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -19,10 +19,20 @@ public class DemoSecurityConfig {
 
     //ADD SUPPORT FOR JDB... no more hardcoded user :)
     @Bean
-    public UserDetailsManager userDetailsManager (DataSource dataSource){
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
 
 
-        return new JdbcUserDetailsManager(dataSource);
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+
+        //DEFINE QUERY TO RETRIEVE A USER BY USERNAME
+        jdbcUserDetailsManager.setUsersByUsernameQuery(
+                "select user_id, pw, active from members where user_id=?");
+        //DEFINE QUERRY TO RETRIEVE THE AUTHORITIES/ROLES BY USERNAME
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery(
+                "select user_id, role from roles where user_id=?"
+        );
+
+        return jdbcUserDetailsManager;
     }
 
     @Bean
