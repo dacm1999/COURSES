b69c99e41b8106f5165e073a938e979ea19c46ab
dacm1999 <danielcontreras12.dc@gmail.com>
2024-02-08 16:08:19 +0100

    SPRING BOOT: 171 ADDING DATA TO MODEL

--- 04-SPRING-BOOT-COURSE/06-spring-boot-spring-mvc/03-reading-html-form-data/src/main/java/com/dacm/springboot/thymeleafdemo/controller/HelloWorldController.java
+++ 04-SPRING-BOOT-COURSE/06-spring-boot-spring-mvc/03-reading-html-form-data/src/main/java/com/dacm/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -1,2 +1,20 @@
-package com.dacm.springboot.thymeleafdemo.controller;public class HelloWorldController {
+package com.dacm.springboot.thymeleafdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+
+    //Need a CONTROLLER METHOD to show initial HTML FORM
+    @RequestMapping("/showform")
+    public String showForm(){
+        return "helloworld-form";
+    }
+
+    //NEED A CONTROLLER METHOD TO RPOCESS THE HTML FORM
+    @RequestMapping("/processForm")
+    public String processForm() {
+        return "helloworld";
+    }
 }
---
+++ 04-SPRING-BOOT-COURSE/06-spring-boot-spring-mvc/04-adding-data-to-the-model/src/main/java/com/dacm/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -0,0 +1,42 @@
+package com.dacm.springboot.thymeleafdemo.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+
+    //Need a CONTROLLER METHOD to show initial HTML FORM
+    @RequestMapping("/showform")
+    public String showForm(){
+        return "helloworld-form";
+    }
+
+    //NEED A CONTROLLER METHOD TO RPOCESS THE HTML FORM
+    @RequestMapping("/processForm")
+    public String processForm() {
+        return "helloworld";
+    }
+
+    //ADD A CONTROLLER METHOD TO READ FORM DARA AND ADD DATA TO THE MODEL
+    @RequestMapping("/processFormVersionTwo")
+    public String lestShoutDude(HttpServletRequest request, Model model){
+
+        //read the requesr paramenter from the html from
+        String thename = request.getParameter("studentName");
+
+        //convert the data to all caps
+        thename = thename.toUpperCase();
+
+        // create the message
+        String result = "Yo! " + thename;
+
+        // add messsage to the model
+        model.addAttribute("messageFromModel", result);
+
+        return "helloworld";
+    }
+
+}
