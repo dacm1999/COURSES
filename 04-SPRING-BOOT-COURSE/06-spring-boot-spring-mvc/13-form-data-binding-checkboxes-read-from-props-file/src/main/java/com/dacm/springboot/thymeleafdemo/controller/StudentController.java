b3d2fd134ef4db4caff0b8e81e216420a3aa6919
dacm1999 <danielcontreras12.dc@gmail.com>
2024-02-12 15:22:37 +0100

    SPRING BOOT: 189 DATA BINDING - CHECK BOXES

---
+++ 04-SPRING-BOOT-COURSE/06-spring-boot-spring-mvc/13-form-data-binding-checkboxes-read-from-props-file/src/main/java/com/dacm/springboot/thymeleafdemo/controller/StudentController.java
@@ -0,0 +1,45 @@
+package com.dacm.springboot.thymeleafdemo.controller;
+
+import com.dacm.springboot.thymeleafdemo.model.Student;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.awt.*;
+import java.util.List;
+
+@Controller
+public class StudentController {
+
+    @Value("${languages}")
+    private List<String> languages;
+
+    @Value("${countries}")
+    private List<String> countries;
+
+    @GetMapping("/showStudentsForm")
+    public String showForm(Model model){
+
+        //Create a student object
+        Student theStudent = new Student();
+        //Add student ibject to the model
+        model.addAttribute("student",theStudent);
+        //Add the list of countries to the model
+        model.addAttribute("countries",countries);
+        //Add the list of languages to the model
+        model.addAttribute("languages",languages);
+        return "student-form";
+    }
+
+    @PostMapping("/processStudentForm")
+    public String processForm(@ModelAttribute("student") Student theStudent){
+
+        //Log the input data
+        System.out.println("The student: " + theStudent.getFirstName() + " " +theStudent.getLastName());
+
+        return "student-confirmation";
+    }
+}
