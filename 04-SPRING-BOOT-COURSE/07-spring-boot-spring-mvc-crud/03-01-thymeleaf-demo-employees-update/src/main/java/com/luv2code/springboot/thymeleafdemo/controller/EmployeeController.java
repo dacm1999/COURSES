4138031d1eb96805e4105a9c6fedfef389144c78
dacm1999 <danielcontreras12.dc@gmail.com>
2024-03-13 17:56:03 +0100

    SPRING BOOT 225 CRUD -> ADD EMPLOYEES

---
+++ 04-SPRING-BOOT-COURSE/07-spring-boot-spring-mvc-crud/03-01-thymeleaf-demo-employees-update/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -0,0 +1,55 @@
+package com.luv2code.springboot.thymeleafdemo.controller;
+
+import com.luv2code.springboot.thymeleafdemo.entity.Employee;
+import com.luv2code.springboot.thymeleafdemo.service.implementedService.EmployeeServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/employees")
+@RequiredArgsConstructor
+public class EmployeeController {
+
+    private final EmployeeServiceImpl employeeService;
+
+    @GetMapping("/list")
+    public String listEmployee(Model themodel){
+        //Get the employees from db
+        List<Employee> theEmployees = employeeService.findAll();
+
+        themodel.addAttribute("employees",theEmployees);
+        //Add to the spring model
+
+
+        return "employees/list-employees";
+    }
+
+    @GetMapping("/showFormForAdd")
+    public String showFormForAdd(Model theModel) {
+
+        // create model attribute to bind form data
+        Employee theEmployee = new Employee();
+
+        theModel.addAttribute("employee", theEmployee);
+
+        return "employees/employee-form";
+    }
+
+    @PostMapping("/save")
+    public String saveEmployee(@ModelAttribute("employee") Employee theEmployee) {
+
+        // save the employee
+        employeeService.save(theEmployee);
+
+        // use a redirect to prevent duplicate submissions
+        return "redirect:/employees/list";
+    }
+
+}
---
+++ 04-SPRING-BOOT-COURSE/07-spring-boot-spring-mvc-crud/03-01-thymeleaf-demo-employees-update/src/main/java/com/luv2code/springboot/thymeleafdemo/repository/EmployeeRepository.java
@@ -0,0 +1,12 @@
+package com.luv2code.springboot.thymeleafdemo.repository;
+
+import com.luv2code.springboot.thymeleafdemo.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+    List<Employee> findAllByOrderByLastNameAsc();
+
+}
