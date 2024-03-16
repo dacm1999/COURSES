365aebcbbbe3f24c86dc34ca15e4c77916d4e073
dacm1999 <danielcontreras12.dc@gmail.com>
2024-03-16 15:01:29 +0100

    SPRING BOOT JPA/HIBERNATE ADVANCE MAPPINGS 301 -> ONE TO MANY

---
+++ 04-SPRING-BOOT-COURSE/09-spring-boot-jpa-advanced-mappings/08-jpa-one-to-many-eager-find-instructor-with-courses/src/main/java/com/dacm/cruddemo/entity/Course.java
@@ -0,0 +1,36 @@
+package com.dacm.cruddemo.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+
+@Entity
+@Table(name = "course")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Course {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "title")
+    private String title;
+
+    @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE,
+            CascadeType.DETACH, CascadeType.REFRESH})
+    @JoinColumn(name="instructor_id")
+    private Instructor instructor;
+
+    public Course(String title) {
+        this.title = title;
+    }
+
+}
