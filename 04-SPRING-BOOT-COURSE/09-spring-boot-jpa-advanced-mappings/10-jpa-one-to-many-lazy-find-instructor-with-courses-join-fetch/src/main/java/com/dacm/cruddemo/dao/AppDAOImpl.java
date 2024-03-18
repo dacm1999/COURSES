9626e44d0e5c805052a39a5c8ffe0df556bea2c7
dacm1999 <danielcontreras12.dc@gmail.com>
2024-03-18 18:29:12 +0100

    SPRING BOOT JPA/HIBERNATE ADVANCE MAPPINGS 304 -> FETCH TYPES EAGER VS LAZY

---
+++ 04-SPRING-BOOT-COURSE/09-spring-boot-jpa-advanced-mappings/10-jpa-one-to-many-lazy-find-instructor-with-courses-join-fetch/src/main/java/com/dacm/cruddemo/dao/AppDAOImpl.java
@@ -0,0 +1,60 @@
+package com.dacm.cruddemo.dao;
+
+import com.dacm.cruddemo.entity.Instructor;
+import com.dacm.cruddemo.entity.InstructorDetail;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+public class AppDAOImpl implements AppDAO {
+
+    // define field for entity manager
+    private EntityManager entityManager;
+
+    // inject entity manager using constructor injection
+    @Autowired
+    public AppDAOImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    @Transactional
+    public void save(Instructor theInstructor) {
+        entityManager.persist(theInstructor);
+    }
+
+    @Override
+    @Transactional
+    public void deleteInstructorById(int id) {
+        Instructor instructor = entityManager.find(Instructor.class,id);
+        entityManager.remove(instructor);
+    }
+
+    @Override
+    @Transactional
+    public void deleteInstructorDetailById(int id) {
+        InstructorDetail instructorDetail = entityManager.find(InstructorDetail.class, id);
+
+        instructorDetail.getInstructor().setInstructorDetail(null);
+        entityManager.remove(instructorDetail);
+    }
+
+    @Override
+    public InstructorDetail findInstructorDetailById(int id) {
+        return entityManager.find(InstructorDetail.class, id);
+    }
+
+    @Override
+    public Instructor findInstructorById(int theId) {
+        return entityManager.find(Instructor.class, theId);
+    }
+}
+
+
+
+
+
+
+
