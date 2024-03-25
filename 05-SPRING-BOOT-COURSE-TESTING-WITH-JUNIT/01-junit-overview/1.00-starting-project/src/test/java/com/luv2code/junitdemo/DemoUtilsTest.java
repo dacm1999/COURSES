6e0b2221a28868eb52229e98deec17d00cf41aea
dacm1999 <danielcontreras12.dc@gmail.com>
2024-03-25 17:24:14 +0100

    SPRING BOOT Unit Testing - > Test Case - Lifecycle methods

--- 05-SPRING-BOOT-COURSE-TESTING-WITH-JUNIT/01-junit-overview/1.00-starting-project/src/test/java/com/luv2code/junitdemo/DemoUtilsTest.java
+++ 05-SPRING-BOOT-COURSE-TESTING-WITH-JUNIT/01-junit-overview/1.00-starting-project/src/test/java/com/luv2code/junitdemo/DemoUtilsTest.java
@@ -1,23 +1,45 @@
 package com.luv2code.junitdemo;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class DemoUtilsTest {
 
 
+    DemoUtils demoUtils;
+
+    @BeforeEach
+    void setupBeforeEach(){
+        demoUtils = new DemoUtils();
+        System.out.println("@BeforeEach excuses before the execution of each test method");
+    }
+    @AfterEach
+    void tearDownAfterEach(){
+        System.out.println("Running @AfterEach");
+        System.out.println("");
+    }
+
+    @BeforeAll
+    static void setupBeforeEachClass(){
+        System.out.println("@BeforeAll excuses only once before all test method execution in the class");
+    }
+    @AfterAll
+    static void setupAfterAll(){
+        System.out.println("@AfterAll excuses only once after all test methods execution in the class");
+    }
+    
     @Test
     void testEqualsAndNotEquals(){
 
-        DemoUtils demoUtils = new DemoUtils();
+        System.out.println("Running test: testEqualsAndNotEquals");
 
         assertEquals(6, demoUtils.add(2,4), "2 + 4 must be 6");
         assertNotEquals(6, demoUtils.add(1,9), "1+9 must not be 6");
     }
     @Test
     void testNullAndNotNull(){
-        DemoUtils demoUtils = new DemoUtils();
+        System.out.println("Running test: testNullAndNotNull");
 
         String str1 = null;
         String str2 = "DanielContreras";
