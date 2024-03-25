9a2f69461d32344c42666693c8d208ca4ded1755
dacm1999 <danielcontreras12.dc@gmail.com>
2024-03-25 17:42:08 +0100

    SPRING BOOT Unit Testing - > Display Names in test methods

--- 05-SPRING-BOOT-COURSE-TESTING-WITH-JUNIT/01-junit-overview/1.00-starting-project/src/test/java/com/luv2code/junitdemo/DemoUtilsTest.java
+++ 05-SPRING-BOOT-COURSE-TESTING-WITH-JUNIT/01-junit-overview/1.00-starting-project/src/test/java/com/luv2code/junitdemo/DemoUtilsTest.java
@@ -4,43 +4,28 @@ import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayNameGeneration(DisplayNameGenerator.Simple.class)
 class DemoUtilsTest {
 
 
     DemoUtils demoUtils;
 
+
     @BeforeEach
     void setupBeforeEach(){
         demoUtils = new DemoUtils();
-        System.out.println("@BeforeEach excuses before the execution of each test method");
-    }
-    @AfterEach
-    void tearDownAfterEach(){
-        System.out.println("Running @AfterEach");
-        System.out.println("");
     }
 
-    @BeforeAll
-    static void setupBeforeEachClass(){
-        System.out.println("@BeforeAll excuses only once before all test method execution in the class");
-    }
-    @AfterAll
-    static void setupAfterAll(){
-        System.out.println("@AfterAll excuses only once after all test methods execution in the class");
-    }
-    
     @Test
+    @DisplayName("Equals and not equals")
     void testEqualsAndNotEquals(){
-
-        System.out.println("Running test: testEqualsAndNotEquals");
-
         assertEquals(6, demoUtils.add(2,4), "2 + 4 must be 6");
         assertNotEquals(6, demoUtils.add(1,9), "1+9 must not be 6");
     }
-    @Test
-    void testNullAndNotNull(){
-        System.out.println("Running test: testNullAndNotNull");
 
+    @Test
+    @DisplayName("Null and NotNull")
+    void testNullAndNotNull(){
         String str1 = null;
         String str2 = "DanielContreras";
 
@@ -48,4 +33,21 @@ class DemoUtilsTest {
         assertNotNull(demoUtils.checkNull(str2), "Object should n be null");
 
     }
+
+
+//    @AfterEach
+//    void tearDownAfterEach(){
+//        System.out.println("Running @AfterEach");
+//        System.out.println("");
+//    }
+//
+//    @BeforeAll
+//    static void setupBeforeEachClass(){
+//        System.out.println("@BeforeAll excuses only once before all test method execution in the class");
+//    }
+
+//    @AfterAll
+//    static void setupAfterAll(){
+//        System.out.println("@AfterAll excuses only once after all test methods execution in the class");
+//    }
 }
