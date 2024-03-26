5a52c261ac721f04b54270f1e66efdcb3fd7260b
dacm1999 <danielcontreras12.dc@gmail.com>
2024-03-26 22:37:36 +0100

    SPRING BOOT Unit Testing - > JUnit Assertions - Arrays, Iterable and Lines

--- 05-SPRING-BOOT-COURSE-TESTING-WITH-JUNIT/01-junit-overview/1.00-starting-project/src/test/java/com/luv2code/junitdemo/DemoUtilsTest.java
+++ 05-SPRING-BOOT-COURSE-TESTING-WITH-JUNIT/01-junit-overview/1.00-starting-project/src/test/java/com/luv2code/junitdemo/DemoUtilsTest.java
@@ -2,6 +2,8 @@ package com.luv2code.junitdemo;
 
 import org.junit.jupiter.api.*;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 //@DisplayNameGeneration(DisplayNameGenerator.Simple.class)
@@ -49,6 +51,29 @@ class DemoUtilsTest {
 
         assertTrue(demoUtils.isGreater(grandeOne,gradeTwo),"This should return true");
         assertFalse(demoUtils.isGreater(gradeTwo,grandeOne),"This should return false");
+    }
+
+    @Test
+    @DisplayName("Array equals")
+    void testArrayEquals(){
+        String [] stringArray = {"A", "B", "C"};
+        assertArrayEquals(stringArray,demoUtils.getFirstThreeLettersOfAlphabet(),"Arrays should be equals");
+    }
+
+    @Test
+    @DisplayName("Iterable equals")
+    void testIterableEquals(){
+        List<String> theList = List.of("luv","2","code");
+
+        String [] stringArray = {"luv", "2", "code"};
+        assertIterableEquals(theList,demoUtils.getAcademyInList(),"Expected list should be same as actual list");
+    }
+
+    @Test
+    @DisplayName("Lines match")
+    void testLinesMatch(){
+        List<String> theList = List.of("luv","2","code");
+        assertLinesMatch(theList, demoUtils.getAcademyInList(),"Lines should match");
     }
 
 
